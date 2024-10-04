4a43f8d66e715bde3b471df1e47857b52018d92d
Amit <amitthecoders@gmail.com>
2024-10-04 09:22:14 +0530

    Watermalon.kt

---
+++ Watermalon.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val x = readln().toInt()
+    if(x>2){
+        if(x%2==0){
+            print("YES")
+        }
+        else{
+            print("NO")
+        }
+    }
+    else{
+        print("NO")
+    }
+}
